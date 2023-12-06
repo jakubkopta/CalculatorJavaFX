853f8ff5b1794a595a86ddee0559d75e0ba0178f
Jakub Kopta <jakubkopta@gmail.com>
2023-12-06 11:30:48 +0100

    Symbols filled + style

--- src/main/java/com/example/calculatorjavafx/HelloController.java
+++ src/main/java/com/example/calculatorjavafx/HelloController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+
 
 public class HelloController {
     @FXML
@@ -17,7 +17,7 @@ public class HelloController {
     @FXML
     void onNumberClicked(MouseEvent event) {
         int value = Integer.parseInt(((Button)event.getSource()).getId().replace("btn", ""));
-        display.setText(Double.parseDouble(display.getText())==0?String.valueOf((double)value):String.valueOf(Double.parseDouble(display.getText())*10+value));
+        display.setText(Double.parseDouble(display.getText())<=0?String.valueOf((double)value):String.valueOf(Double.parseDouble(display.getText())*10+value));
     }
 
     @FXML
@@ -33,10 +33,19 @@ public class HelloController {
             }
             operator = ".";
         }
-        else if(symbol.equals("AC")) {
-            display.setText(String.valueOf(0.0));
+        else if (symbol.equals("AC")) {
+            display.setText(String.valueOf(0));
             operator = ".";
         }
+        else if (symbol.equals("Switch")) {
+            display.setText(Double.parseDouble(display.getText())==0? display.getText(): String.valueOf(Double.parseDouble(display.getText())*(-1)));
+        }
+        else if (symbol.equals("Percent")) {
+            display.setText(Double.parseDouble(display.getText())==0? display.getText(): String.valueOf(Double.parseDouble(display.getText())*0.01));
+        }
+//        else if (symbol.equals("Coma")) {
+//            display.setText(display.getText() + ".");
+//        }
         else {
             switch (symbol) {
                 case "Plus" -> operator = "+";
@@ -45,7 +54,7 @@ public class HelloController {
                 case "Divide" -> operator = "/";
             }
             num1 = Double.parseDouble(display.getText());
-            display.setText(String.valueOf(0.0));
+            display.setText(String.valueOf(0));
         }
     }
 
