426fc3a4cb5c0fc9f844d01eb29ba299c5de9fc1
jakubkopta <jakubkopta@gmail.com>
2023-12-11 14:44:58 +0100

    comma button and icon

--- src/main/java/com/example/calculatorjavafx/HelloApplication.java
+++ src/main/java/com/example/calculatorjavafx/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.calculatorjavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,8 @@ import java.util.Objects;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
         stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")))));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("calculator-icon.png"))));
         stage.setTitle("Calculator");
         stage.setResizable(false);
         stage.show();
--- src/main/java/com/example/calculatorjavafx/HelloController.java
+++ src/main/java/com/example/calculatorjavafx/HelloController.java
@@ -17,44 +17,56 @@ public class HelloController {
     @FXML
     void onNumberClicked(MouseEvent event) {
         int value = Integer.parseInt(((Button)event.getSource()).getId().replace("btn", ""));
-        display.setText(Double.parseDouble(display.getText())<=0?String.valueOf((double)value):String.valueOf(Double.parseDouble(display.getText())*10+value));
+        if (Double.parseDouble(display.getText()) <= 0 || display.getText().contains(".")) {
+            if (display.getText().equals("0")) {
+                display.setText(String.valueOf(value));
+            } else {
+                display.setText(display.getText() + value);
+            }
+        } else {
+            display.setText(display.getText() + value);
+        }
     }
 
     @FXML
     void onSymbolClicked(MouseEvent event) {
         String symbol = ((Button)event.getSource()).getId().replace("btn","");
-        if(symbol.equals("Equal")) {
-            double num2 = Double.parseDouble(display.getText());
-            switch (operator) {
-                case "+" -> display.setText((num1+num2) + "");
-                case "-" -> display.setText((num1-num2) + "");
-                case "*" -> display.setText((num1*num2) + "");
-                case "/" -> display.setText((num1/num2) + "");
+        switch (symbol) {
+            case "Equal" -> {
+                double num2 = Double.parseDouble(display.getText());
+                switch (operator) {
+                    case "+" -> display.setText((num1 + num2) + "");
+                    case "-" -> display.setText((num1 - num2) + "");
+                    case "*" -> display.setText((num1 * num2) + "");
+                    case "/" -> display.setText((num1 / num2) + "");
+                }
+                operator = ".";
             }
-            operator = ".";
-        }
-        else if (symbol.equals("AC")) {
-            display.setText(String.valueOf(0));
-            operator = ".";
-        }
-        else if (symbol.equals("Switch")) {
-            display.setText(Double.parseDouble(display.getText())==0? display.getText(): String.valueOf(Double.parseDouble(display.getText())*(-1)));
-        }
-        else if (symbol.equals("Percent")) {
-            display.setText(Double.parseDouble(display.getText())==0? display.getText(): String.valueOf(Double.parseDouble(display.getText())*0.01));
-        }
-//        else if (symbol.equals("Coma")) {
-//            display.setText(display.getText() + ".");
-//        }
-        else {
-            switch (symbol) {
-                case "Plus" -> operator = "+";
-                case "Minus" -> operator = "-";
-                case "Multiply" -> operator = "*";
-                case "Divide" -> operator = "/";
+            case "AC" -> {
+                display.setText(String.valueOf(0));
+                operator = ".";
             }
-            num1 = Double.parseDouble(display.getText());
-            display.setText(String.valueOf(0));
+            case "Switch" -> {
+                double currentValue = Double.parseDouble(display.getText());
+                display.setText(currentValue == 0 ? display.getText() : String.valueOf(-currentValue).replaceAll("\\.0*$", ""));
+            }
+            case "Percent" ->
+                    display.setText(Double.parseDouble(display.getText()) == 0 ? display.getText() : String.valueOf(Double.parseDouble(display.getText()) * 0.01).replaceAll("\\.0*$", ""));
+            case "Comma" -> {
+                if (!display.getText().contains(".")) {
+                    display.setText(display.getText() + ".");
+                }
+            }
+            default -> {
+                switch (symbol) {
+                    case "Plus" -> operator = "+";
+                    case "Minus" -> operator = "-";
+                    case "Multiply" -> operator = "*";
+                    case "Divide" -> operator = "/";
+                }
+                num1 = Double.parseDouble(display.getText());
+                display.setText(String.valueOf(0));
+            }
         }
     }
 
